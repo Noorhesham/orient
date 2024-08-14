9f988f4e7a7f619221407cab64abdc7366a57ee3
Noorhesham <noordragon2004@gmail.com>
2024-08-14 18:09:35 +0200

    oreint

--- app/[locale]/(paddWrapper)/blog/page.tsx
+++ app/[locale]/(paddWrapper)/blog/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MaxWidthWrapper from "../../../components/MaxWidthWrapper";
 import Heading from "../../../components/Heading";
 import CardHuge from "../../../components/CardHuge";
+import MotionContainer from "@/app/components/MotionContainer";
 
 const page = () => {
   return (
@@ -13,7 +14,7 @@ const page = () => {
             subText="articles about paints"
             paragraph=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsam cum quam ullam, nulla iure nesciunt a, tempora, obcaecati excepturi eos rem tenetur dolor. Ducimus praesentium sapiente provident voluptates officia?"
           />
-          <section className=" gap-4 grid grid-cols-3">
+          <MotionContainer className=" gap-4 grid grid-cols-1 lg:grid-cols-3">
             <CardHuge href="/blog/1" />
             <CardHuge />
             <CardHuge />
@@ -23,7 +24,7 @@ const page = () => {
             <CardHuge />
             <CardHuge />
             <CardHuge />
-          </section>
+          </motionconta>
         </MaxWidthWrapper>
       </section>
 
