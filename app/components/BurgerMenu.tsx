ca51f2bdce62dc13b924012eb31402f8282465fa
Noorhesham <noordragon2004@gmail.com>
2024-08-14 18:08:26 +0200

    oreint

--- app/components/BurgerMenu.tsx
+++ app/components/BurgerMenu.tsx
@@ -37,15 +37,16 @@ const BurgerMenu = ({ links }: { links: any[] }) => {
   }, [isOpen]);
   return (
     <motion.nav
-      className={` w-0   h-full absolute inset-0 ${white ? "bg-white     overflow-y-scroll" : ""} ${
-        isOpen ? "w-[300px] " : "w-0"
-      }`}
+      className={` w-0   h-full absolute inset-0 ${white ? "bg-white     overflow-y-scroll" : ""} `}
       initial={false}
       animate={isOpen ? "open" : "closed"}
       custom={height}
       ref={containerRef}
     >
-      <motion.div className={`    absolute top-0 left-0  bottom-0 min-h-full  w-[300px] bg-white`} variants={sidebar} />
+      <motion.div
+        className={`    absolute top-0 left-0  bottom-0 min-h-full ${isOpen ? "w-[300px] " : "w-0"} bg-white`}
+        variants={sidebar}
+      />
       <Navigation isOpen={isOpen} links={links} />
       <MenuToggle toggle={() => toggleOpen()} />
     </motion.nav>
