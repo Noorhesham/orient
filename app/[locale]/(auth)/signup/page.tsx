f8ec1be7567f7b08b8013ac29621d5d8afe27077
Noorhesham <noordragon2004@gmail.com>
2024-09-30 10:12:36 +0200

    gotoshop

--- app/[locale]/(auth)/signup/page.tsx
+++ app/[locale]/(auth)/signup/page.tsx
@@ -1,9 +1,7 @@
 import Signup from "@/app/main/authentication/components/SignUp";
-import { Server } from "@/app/main/Server";
 import React from "react";
 
 const page = async () => {
-  const res = Server({ resourceName: "MGS", body: { needed: ["general_settings"] }, cache: true });
   return <Signup />;
 };
 
