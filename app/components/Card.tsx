62c746fc3142f781f48f05741aeaa376bba5a8b4
Noorhesham <noordragon2004@gmail.com>
2024-10-30 08:22:31 +0200

    seo

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -26,8 +26,8 @@ const Card = ({ img, text, children, width, className, price, id, sell, desc }: 
         before:absolute before:border-b-2 before:border-gray-50 before:scale-y-0 group-hover:before:scale-y-100   before:duration-300 before:right-0 before:top-5 before:border-r-2 before:w-full before:h-full
        after:border-gray-50 after:scale-x-0 group-hover:after:scale-x-100  after:duration-300 after:left-0 after:top-0 after:w-full after:h-full p-2 group-hover:after:border-l-2 top-1/2 h-fit left-1/2 -translate-x-1/2 -translate-y-1/2  w-full z-30 max-w-[14rem]  group-hover:opacity-100 opacity-0 transition-opacity duration-300"
       >
-        {<p className="text-sm  font-semibold mb-1 text-main text-center  z-30">{text}</p>}
-        {desc && <p className="text-xs text-center text-gray-50 line-clamp-5 z-30">{desc}</p>}
+        {<p className="md:text-sm  text-xs  font-semibold mb-1 text-main text-center  z-30">{text}</p>}
+        {desc && <p className="md:text-xs  text-xs text-center text-gray-50 line-clamp-5 z-30">{desc}</p>}
       </div>
       <Link href={id ? `/product/${id}` : "#"} className="flex flex-col w-full items-center">
         <div className={`${width ? width : "aspect-[403/400]"} h-auto mb-auto self-start w-full relative`}>
