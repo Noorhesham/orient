a3aee0ceda8b206e818e1b46fe93e49ae84bb416
Noorhesham <noordragon2004@gmail.com>
2024-11-17 12:26:32 +0200

    margin input

--- app/components/OrderDetails.tsx
+++ app/components/OrderDetails.tsx
@@ -26,7 +26,7 @@ const OrderDetails = ({ id }: { id: string }) => {
   return (
     <div className="lg:px-10 py-10 lg:py-5">
       <MiniHeader
-        heading={t("orderNumber", { uuid: order?.uuid })} // Use translation with variable
+        heading={t("orderNumber", { uuid: order?.uuid })} 
         date={format(new Date(order.date), "dd/MM/yyyy")}
         status={t1(order.status)}
       />
