c105e6c395c0fdf0feb34c0e49aa1bc1c5c102b7
Noorhesham <noordragon2004@gmail.com>
2025-01-20 10:27:17 +0200

    dr nurse edits

--- app/main/authentication/components/ResetPassword.tsx
+++ app/main/authentication/components/ResetPassword.tsx
@@ -56,9 +56,9 @@ const ResetPassword = () => {
 
   return (
     <Suspense fallback={<Spinner />}>
-      <Section CustomePadding="px-5 py-40" className=" bg-gray-50 justify-center flex flex-1 flex-col items-center">
+      <Section className=" bg-gray-50 justify-center flex flex-1 flex-col items-center">
         <div className=" mx-auto flex flex-col items-center justify-center w-full">
-          <Logo  type="blue" />
+          <Logo type="blue" />
           <h3 className=" text-center text-2xl mt-8 font-bold text-main2">{t("forgotPasswordContent.title")}</h3>
           {param === "prepare" && <Prepare setMessage={setMessage} handleParam={handleParam} setMethods={setMethods} />}
           {param === "forgot" && (
