9db78f498b4e7e7336d7a399673b8ee95e4f0078
Noorhesham <noordragon2004@gmail.com>
2025-02-24 10:40:31 +0200

    notification token

--- app/main/authentication/components/Login.tsx
+++ app/main/authentication/components/Login.tsx
@@ -148,7 +148,19 @@ const Login = () => {
       const token = searchParams.get("token");
       if (token) {
         toast.success(searchParams.get("message"));
-        if (token) cookies.set("jwt", token || "", { expires: 2 });
+        if (token) {
+          cookies.set("jwt", token || "", { expires: 2 });
+          const NotificationREs = await Server({
+            resourceName: "languageUpdate",
+            body: {
+              action: "set",
+              key: "notification_token_status",
+              value: true,
+              device_info: deviceInfo,
+            },
+          });
+          console.log(NotificationREs);
+        }
         setLogin((l) => !l);
         router.push(redirect || "/");
       }
